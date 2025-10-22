5e85128eab20f0d8a2b098713f5a530783fbcc03
Germán G. Lugo Muñoz <glugo-mu@student.42firenze.it>
2025-10-22 13:57:30 +0200

    Improve mutex over lastmeal.

--- philo/main.c
+++ philo/main.c
@@ -6,16 +6,18 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 17:28:08 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/10/22 12:51:46 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/10/22 13:52:54 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	start_meal(t_philo	*philo)
+void	start_routine(t_philo	*philo)
 {
 	print_action(philo, "is eating");
+	pthread_mutex_lock(&philo->config->meals_lock);
 	philo->last_meal = chrono_lap(&philo->config->t);
+	pthread_mutex_unlock(&philo->config->meals_lock);
 	usleep(philo->config->time_to_eat * 1000);
 	print_action(philo, "is sleeping");
 	usleep(philo->config->time_to_sleep * 1000);
@@ -40,7 +42,7 @@ void	*philosophers_life(void *args)
 		if (are_meals_complete(philo))
 			break ;
 		if (try_eat(philo))
-			start_meal(philo);
+			start_routine(philo);
 		else
 			usleep(100);
 	}
@@ -51,6 +53,7 @@ void	init_philosophers(t_config *config, t_philo *philo)
 {
 	int	i;
 
+	config->philosophers = philo;
 	i = 0;
 	while (i < config->num_of_philosophers)
 	{
@@ -64,6 +67,7 @@ void	init_philosophers(t_config *config, t_philo *philo)
 		pthread_create(&philo[i].thread, NULL, philosophers_life, &philo[i]);
 		i++;
 	}
+	pthread_create(&config->monitor_thread, NULL, monitor_philosophers, config);
 	return ;
 }
 
@@ -71,6 +75,7 @@ void	start_dinner(t_config *config, t_philo *philo)
 {
 	int	i;
 
+	pthread_join(config->monitor_thread, NULL);
 	i = 0;
 	while (i < config->num_of_philosophers)
 	{
